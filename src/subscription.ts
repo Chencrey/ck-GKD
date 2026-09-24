81500127b94f76e875eeb3e4bc793ae80399eebf
chencrey <chencrey@gmail.com>
2026-09-25 03:01:39 +0800

    中国广东、小度、中国移动、红果免费短剧、飞猪旅行、小米应用商店、智联招聘、哔哩哔哩

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,9 +5,9 @@ import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
   id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  name: 'ck‘s Subscription',
+  version: 1,
+  author: 'Chencrey',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
