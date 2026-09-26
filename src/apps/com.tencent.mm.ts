2f2764b475d2bbf80bf3c34e080e4b7463f4ef62
chencrey <chencrey@gmail.com>
2026-09-27 01:43:41 +0800

    中国广东、小度、中国移动、红果免费短剧、飞猪旅行、小米应用商店、智联招聘、哔哩哔哩、小米运动健康、小米商城、同程旅行、南方航空、美团外卖、Keep、航旅纵横、公考雷达、东方航空、聪明卫士、菜鸟、北京一卡通、百度网盘、大华云联

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -3,5 +3,56 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [],
+  groups: [
+    {
+      key: 1,
+      name: '微信扫码自动点登录',
+      desc: '微信扫码自动点登录',
+      rules: [
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.plugin.webwx.ui.ExtDeviceWXLoginUI',
+          matches: '[vid="pjb"]',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '微信自动选发原图',
+      desc: '微信自动选发原图',
+      rules: [
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: '.plugin.gallery.ui.AlbumPreviewUI',
+          matches: '[vid="km5"]',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '微信自动点查看原图',
+      desc: '微信自动点查看原图',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.ui.LauncherUI',
+          matches: '[vid="ug7"]',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '微信自动点查看原视频',
+      desc: '微信自动点查看原视频',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.ui.LauncherUI',
+          matches: '[vid="ug8"]',
+        },
+      ],
+    },
+  ],
 });
